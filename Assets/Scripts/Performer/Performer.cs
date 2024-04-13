475ac9ff2898ac6cf93af93e5e42f27cee950dad
TWTwoFace <pipo2017@mail.ru>
2024-04-13 22:58:27 +0300

    feat: included performers

--- Assets/Scripts/Performer/Performer.cs
+++ Assets/Scripts/Performer/Performer.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class Performer : MonoBehaviour
 {
     private Transform _targetTransform;
-    
+    private EmergencyCar _car;
+
+    private void Awake()
+    {
+        _car = GetComponent<EmergencyCar>();
+    }
+
     public void SetTask(Transform _taskTransform)
     {
         _targetTransform = _taskTransform;
-        // ...
+        _car.Initialize(_targetTransform.position);
     }
 }
