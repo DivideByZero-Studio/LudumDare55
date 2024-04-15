e9920b70fd2b276644ced720d0ce1b5db90e84f8
TWTwoFace <pipo2017@mail.ru>
2024-04-15 04:01:37 +0300

    feat: added people like add method in people like service

--- Assets/Scripts/PeopleLikeService/PeopleLikeService.cs
+++ Assets/Scripts/PeopleLikeService/PeopleLikeService.cs
@@ -9,6 +9,8 @@ public class PeopleLikeService : MonoBehaviour
     public int CurrentLikes => _currentLikes;
 
     [SerializeField] private int _maxLikes;
+    [SerializeField] private int _addCountLikes;
+    [SerializeField] private int _decreaseCountLikes;
 
     [Inject] private ChooseServicePanel _chooseServicePanel;
 
@@ -24,23 +26,31 @@ public class PeopleLikeService : MonoBehaviour
         if (_currentLikes <= 0)
             return;
 
-        _currentLikes--;
+        _currentLikes = Math.Clamp(_currentLikes - _decreaseCountLikes, 0, _maxLikes);
         OnLikesChanged?.Invoke();
 
         if (_currentLikes == 0)
-        {
             OnZeroLikes?.Invoke();
-            Debug.Log("q");
-        }
+    }
+
+    private void AddLike(Transform iconTransform, EmergencyType type)
+    {
+        if (_currentLikes == _maxLikes)
+            return;
+
+        _currentLikes = Math.Clamp(_currentLikes + _addCountLikes, 0, _maxLikes);
+        OnLikesChanged?.Invoke();
     }
 
     private void OnEnable()
     {
         _chooseServicePanel.EmergencyFailed += DecreaseLike;
+        _chooseServicePanel.EmergencyDone += AddLike;
     }
 
     private void OnDisable()
     {
         _chooseServicePanel.EmergencyFailed -= DecreaseLike;
+        _chooseServicePanel.EmergencyDone -= AddLike;
     }
 }
